6afb6d637919cce9205c2ffeee49f04a700a9f70
JarritB <jarrit.boons@outlook.com>
2019-03-13 12:39:37 +0100

    Add working version of Line following node

--- catkin_ws/src/Line_Follower_pkg/LineFollowerNode.py
+++ catkin_ws/src/Line_Follower_pkg/LineFollowerNode.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python2.7
+
 import rospy
 from sensor_msgs.msg import Image
 import cv2, cv_bridge, numpy
+from geometry_msgs.msg import Twist
 class Follower:
 	def __init__(self):
 		self.bridge = cv_bridge.CvBridge()
 		cv2.namedWindow("window", 1)
 		cv2.waitKey(3)
-		self.image_sub = rospy.Subscriber('/image_publisher_1552472555706544959/image_raw',Image, self.image_callback) #/usb_cam/image_raw/Image
+		self.image_sub = rospy.Subscriber('/usb_cam/image_raw',Image, self.image_callback) #/usb_cam/image_raw/Image
+		self.cmd_vel_pub = rospy.Publisher('cmd_vel_mux/Line_Follower_vel',Twist, queue_size=1)
+		self.twist = Twist()
 	def image_callback(self, msg):
-		image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
+		image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='passthrough')
 		hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-		lower_yellow = numpy.array([ 10, 0, 0])
-		upper_yellow = numpy.array([255, 255, 255])
+		lower_yellow = numpy.array([20, 100, 100])
+		upper_yellow = numpy.array([30, 255, 255])
 		mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
 		h, w, d = image.shape
 		search_top = 3*h/4
@@ -23,15 +27,20 @@ class Follower:
 		if M['m00'] > 0:
 			cx = int(M['m10']/M['m00'])
 			cy = int(M['m01']/M['m00'])
-			cv2.circle(image, (w/2, h/2), 20, (0,0,255), 1)
-			cv2.circle(image, (cx, cy), 5, (0,0,255), -1)
 			err = cx - w/2
+			if(cx > w/2 - 20 and cx < w/2 + 20):
+				color = (50,255,50)
+			else:
+				 color = (0,0,255)
+				 print(err)
+			cv2.circle(image, (w/2, cy), 20, color, 1)
+			cv2.circle(image, (cx, cy), 5, color, -1)
 			self.twist.linear.x = 0.2
 			self.twist.angular.z = -float(err) / 100
 			self.cmd_vel_pub.publish(self.twist)
-			cv2.imshow("window", image)
-			cv2.waitKey(3)
+		cv2.imshow("window", image)
+		cv2.waitKey(3)
 
-rospy.init_node('follower')
+rospy.init_node('Line_Follower_Node')
 follower = Follower()
 rospy.spin()
