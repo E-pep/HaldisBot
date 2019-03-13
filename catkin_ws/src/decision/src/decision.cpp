f568ea90f808edfd563bb5eaf2d5607ea31b8b44
E-pep <paul.leroy1996@hotmail.com>
2019-03-13 19:29:14 +0100

    switch of different states added

--- catkin_ws/src/decision/src/decision.cpp
+++ catkin_ws/src/decision/src/decision.cpp
@@ -4,15 +4,37 @@
 #include <iostream>
 #include <sstream>
 
+/* states
 
+0: waiting on the Aruco order
+1: Move to objects
+
+
+*/
+
+
+int aruco_threshold = 4;
 int detected_aruco;
+int state = 0;
+int previous_state = state;
 
 
 void ArucoCallback(const std_msgs::Int32::ConstPtr& msg)
 {
 	detected_aruco = msg->data;
-	std::cout << "detected:"<< detected_aruco << std::endl;
+
+	if(detected_aruco < aruco_threshold)
+	{
+		std::cout << "detected:"<< detected_aruco << std::endl;
+		previous_state = state;
+		state = 1;	
+	}
+
+
+	
 }
+
+
 
 int main(int argc, char **argv)
 {
@@ -20,10 +42,38 @@ int main(int argc, char **argv)
 
 	ros::init(argc, argv, "listener");
 	ros::NodeHandle n;
+	ros::Subscriber sub;
+	sub = n.subscribe("/id_pub", 1, ArucoCallback);
 
-	ros::Subscriber sub = n.subscribe("/id_pub", 1000, ArucoCallback);
+	while(1)
+	{
+		// switch case for state machine 
+	 if(state != previous_state)
+	 {
+		switch(state) 
+		{
+		case 0 : std::cout << "state 0" << std::endl; 			// waiting for Aruco
+			 sub = n.subscribe("/id_pub", 1, ArucoCallback);	// subscribing to Aruco topic and use callback
+			 break;
 
-	ros::spin();
+    		case 1 : std::cout << "state 1" << std::endl; 			// Move to Object 
+			 sub.shutdown();
+			 previous_state = state;
+			// sub = nh.subscribe("", 1, callbackFunc);
+			 break;        
+    		case 2 : std::cout << "state 2" << std::endl; // 
+             		 break;
+    		case 3 : std::cout << "state 3" << std::endl; // 
+			 break;        
+
+		default: std::cout << "undefined state" << std::endl;
+			 break;
+		}
+
+	 }
+	 ros::spinOnce();
+
+	}
 
 
 	return 0;
