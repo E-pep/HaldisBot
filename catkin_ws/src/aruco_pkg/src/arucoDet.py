d01d119f133aff8f6e654e40dafc0abadd5553f3
WoutVerschaeren <woutverschaeren@telenet.be>
2019-03-27 12:07:45 +0100

    ArUco node stuurt nu de code en de relatieve positie van de meest gecentreerde marker

--- catkin_ws/src/aruco_pkg/src/arucoDet.py
+++ catkin_ws/src/aruco_pkg/src/arucoDet.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python2.7
 import rospy
-from std_msgs.msg import Int32
+from std_msgs.msg import Float32MultiArray
 from sensor_msgs.msg import Image
 import cv2
 from cv2 import aruco
@@ -11,46 +11,61 @@ from cv_bridge import CvBridge
 bridge = CvBridge()
 
 def callback(data):
+    # Convert to cv image
     cv_img = bridge.imgmsg_to_cv2(data, desired_encoding="passthrough")
 
     params = aruco.DetectorParameters_create()
 
+    # Convert the image to gray scale and detect and draw ArUco markers
     gray = cv2.cvtColor(cv_img, cv2.COLOR_BGR2GRAY)
     corners, ids, rejected_img_pts = aruco.detectMarkers(gray, aruco_dict, parameters=params)
-    # img_marker = aruco.drawDetectedMarkers(img.copy(), rejected_img_pts)
     img_marker = aruco.drawDetectedMarkers(cv_img.copy(), corners, ids)
 
+    # Get the width and height of the image
+    height, width = img_marker.shape[:2]
+    # print(width, height)
+
+    # Compute the centers of the detected markers
+    markers = []
+    if ids is not None:
+        for marker, id in zip(corners, ids):
+            # The two opposing corner points: top left and bottom right
+            pt1 = marker[0][0]
+            pt2 = marker[0][2]
+            # print("point1", pt1[0], pt1[1])
+            # print("point2", pt2[0], pt2[1])
+
+            # Average those two points to get the center of the marker
+            center_x = (pt1[0] + pt2[0])/2
+            center_y = (pt1[1] + pt2[1])/2
+            # Convert them relative to the image size
+            center_x_rel = round(center_x/width, 3)
+            center_y_rel = round(center_y/height, 3)
+
+            # Save every marker's center and draw it
+            markers.append([id[0], center_x_rel, center_y_rel])
+            radius = int(abs(pt1[0] - pt2[0])/10)
+            cv2.circle(img_marker, (int(center_x), int(center_y)), radius, (0, 0, 255))
+
+        # Select the marker that's closest to the center
+        most_centered = [200, 1, 1]
+        for marker in markers:
+            # print(marker)
+            if abs(marker[1] - 0.5) < abs(most_centered[1] - 0.5):
+                most_centered = marker
+
+        # print("Most centered", most_centered)
+        # Publish the most centered marker: id, rel_x and rel_y
+        to_publish = Float32MultiArray(data=most_centered)
+        pub.publish(to_publish)
+
+    # Show the image with the detected ArUco markers
     title = "Markers read"
     # print(ids)
     cv2.namedWindow(title, cv2.WINDOW_NORMAL)
-    cv2.resizeWindow(title, 1200, 900)
+    cv2.resizeWindow(title, width*2, height*2)
     cv2.imshow(title, img_marker)
     cv2.waitKey(1)
-
-    width, height = img_marker.shape[:2]
-    print(width, height)
-
-    markers = []
-
-    i = 0
-    for marker in corners:
-        print("marker", i)
-        # The two opposing corner points
-        pt1 = marker[0][0]
-        pt2 = marker[0][2]
-        print("point1", pt1[0], pt1[1])
-        print("point2", pt2[0], pt2[1])
-        center_x = (pt1[0] + pt2[0])/2
-        center_y = (pt1[1] + pt2[1])/2
-        center = [center_x, center_y]
-        print(center)
-        markers.append(center)
-
-        i += 1
-
-    print(markers)
-
-    # pub.publish(ids[0])
 
 
 # help(cv2.aruco)
@@ -66,7 +81,7 @@ aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_100)
 
 
 pub = rospy.Subscriber("/usb_cam/image_raw", Image, callback)
-pub = rospy.Publisher("/id_pub", Int32, queue_size=4)
+pub = rospy.Publisher("/id_pub", Float32MultiArray, queue_size=4)
 rospy.init_node("aruco_det", anonymous=True)
 
 rospy.spin()
--- catkin_ws/src/aruco_pkg/src/arucoIDSubscribe.py
+++ catkin_ws/src/aruco_pkg/src/arucoIDSubscribe.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python2.7
 import rospy
-from std_msgs.msg import Int32
+from std_msgs.msg import Float32MultiArray
 
 
 def callback(data):
-    print(data)
+    print(data.data[0], data.data[1])
 
 
-pub = rospy.Subscriber("/id_pub", Int32, callback)
+pub = rospy.Subscriber("/id_pub", Float32MultiArray, callback)
 rospy.init_node("id_listener", anonymous=True)
 
 rospy.spin()
