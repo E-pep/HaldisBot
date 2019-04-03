7d60016d0ddf20f57b9f4b1e6dbeb005824e2b4d
JarritB <jarrit.boons@outlook.com>
2019-04-03 10:59:46 +0200

    rgb -> bgr?

--- LineFollowerNode.py
+++ LineFollowerNode.py
@@ -10,11 +10,13 @@ class Follower:
 		self.image_sub = rospy.Subscriber('/image_publisher_1552472555706544959/image_raw',Image, self.image_callback) #/usb_cam/image_raw/Image
 	def image_callback(self, msg):
 		image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
-		hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+		h, w, d = image.shape
+		M = cv2.getRotationMatrix2D((cols/2,rows/2),180,1)
+		dst = cv2.warpAffine(image,M,(cols,rows))		
+		hsv = cv2.cvtColor(dst, cv2.COLOR_BGR2HSV)
 		lower_yellow = numpy.array([ 10, 0, 0])
 		upper_yellow = numpy.array([255, 255, 255])
-		mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
-		h, w, d = image.shape
+		mask = cv2.inRange(hsv, lower_yellow, upper_yellow)		
 		search_top = 3*h/4
 		search_bot = search_top + 20
 		mask[0:search_top, 0:w] = 0
